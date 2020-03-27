e5fa7eb5d33bb372b96792c5527afd5fc49f5d45
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2020-03-27 16:22:51 +0000

    Custom commands simplification

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,35 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-Cypress.Commands.add(`getListOfProducts`, () => { 
-    cy.request(`GET`,`v1/products`).then((response) =>{
-        return response
-    });
- });
+Cypress.Commands.add(`getListOfProducts`, () => {
+    return cy.request(`GET`, `v1/products`)
+});
 
- Cypress.Commands.add(`getProductById`, (productId) => { 
-    cy.request(`GET`,`v1/product/${productId}`).then((response) =>{
-        return response
-    })
- })
+Cypress.Commands.add(`getProductById`, (productId) => {
+    return cy.request(`GET`, `v1/product/${productId}`)
+})
 
- Cypress.Commands.add(`putProductById`, (productId) => { 
-    cy.request(`PUT`,`v1/product/${productId}`).then((response) =>{
-        return response
-    })
- })
+Cypress.Commands.add(`putProductById`, (productId) => {
+    return cy.request(`PUT`, `v1/product/${productId}`)
+})
 
- Cypress.Commands.add(`deleteProductById`, (productId) => { 
-    cy.request(`DELETE`,`v1/product/${productId}`).then((response) =>{
-        return response
-    })
- })
+Cypress.Commands.add(`deleteProductById`, (productId) => {
+    return cy.request(`DELETE`, `v1/product/${productId}`)
+})
 
- Cypress.Commands.add(`postProduct`, (name, price) => { 
-    cy.request(`POST`,`v1/product/`, {
+Cypress.Commands.add(`postProduct`, (name, price) => {
+    return cy.request(`POST`, `v1/product`, {
         name: name,
         price: price,
-      }).then((response) =>{
-        return response
     })
- })
+})
