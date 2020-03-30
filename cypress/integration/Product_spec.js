8be6f78f0e70d98dae215034fde515df88de17b9
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2020-03-30 14:50:58 +0100

    Adding PUT tests

--- cypress/integration/Product_spec.js
+++ cypress/integration/Product_spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import moment from 'moment';
+import moment from `moment`;
 const expectedKeys = [`name`, `product_code`, `product_price`];
 const unexpected = [`id`, `name`, `price`]
 
@@ -12,10 +12,10 @@ context(`GET the product list`, () => {
     it(`returns a list of products with the expected properties`, () => {
         cy.getListOfProducts().then((response) => {
             response.body.forEach((item) => {
-                expect(response).property('body').to.exist;
-                expect(item).property('product_price').to.be.a('number')
-                expect(item).property('name').to.be.a('string')
-                expect(item).property('product_code').to.be.a('number')
+                expect(response).property(`body`).to.exist;
+                expect(item).property(`product_price`).to.be.a(`number`)
+                expect(item).property(`name`).to.be.a(`string`)
+                expect(item).property(`product_code`).to.be.a(`number`)
             })
         })
     })
@@ -23,16 +23,16 @@ context(`GET the product list`, () => {
 
 context(`GET a single product by Id`, () => {
     it(`returns a 200 response`, () => {
-        cy.getProductById('1').then((response) => {
+        cy.getProductById(`1`).then((response) => {
             expect(response.status).to.equal(200)
         })
     })
     it(`returns the expected response body properties`, () => {
-        cy.getProductById('1').then((response) => {
-            expect(response).property('body').to.exist
-            expect(response.body).property('product_price').to.be.a('number')
-            expect(response.body).property('name').to.be.a('string')
-            expect(response.body).property('product_code').to.be.a('number')
+        cy.getProductById(`1`).then((response) => {
+            expect(response).property(`body`).to.exist
+            expect(response.body).property(`product_price`).to.be.a(`number`)
+            expect(response.body).property(`name`).to.be.a(`string`)
+            expect(response.body).property(`product_code`).to.be.a(`number`)
         })
     })
 })
@@ -40,28 +40,28 @@ context(`GET a single product by Id`, () => {
 context(`POST a new product`, () => {
     it(`returns a 200 with a valid request`, () => {
         cy.postProduct(`TestPost Item${moment().unix()}`, 12.99).then((response) => {
-            expect(response.status).to.equal(200)           
+            expect(response.status).to.equal(200)
         })
     })
     it(`returns the expected response body properties`, () => {
         cy.postProduct(`TestPost Item${moment().unix()}`, 12.99).then((response) => {
-            expect(response).property('body').to.exist;
-            expect(response.body).property('product_price').to.be.a('number')
-            expect(response.body).property('name').to.be.a('string')
-            expect(response.body).property('product_code').to.be.a('number')
+            expect(response).property(`body`).to.exist;
+            expect(response.body).property(`product_price`).to.be.a(`number`)
+            expect(response.body).property(`name`).to.be.a(`string`)
+            expect(response.body).property(`product_code`).to.be.a(`number`)
         })
     })
     it(`and it shows on the product list`, () => {
-        let testName =`TestPost Item${moment().unix()}`;
+        let testName = `TestPost Item${moment().unix()}`;
         cy.postProduct(testName, 12.99).then((response) => {
-            cy.getListOfProducts().then((response) =>{
-                response.body.forEach((item) =>{
-                    if(item.name.includes(testName)){
-                        expect(item).to.include({name:testName});
-                        expect(item).to.include({price:"12.99"});
+            cy.getListOfProducts().then((response) => {
+                response.body.forEach((item) => {
+                    if (item.name.includes(testName)) {
+                        expect(item).to.include({ name: testName });
+                        expect(item).to.include({ price: "12.99" });
                     }
                 })
-            })        
+            })
         })
     })
     it(`returns a 400 with price sent as a string`, () => {
@@ -72,6 +72,16 @@ context(`POST a new product`, () => {
     it(`returns a 400 with name sent as a integer`, () => {
         cy.postProduct(12345, 12.99).then((response) => {
             expect(response.status).to.equal(400)
+        })
+    })
+})
+
+context.only(`PUT a product`, () => {
+    it(`returns a 200 response`, () => {
+        cy.getProductById(`1`).then((response) => {
+            cy.putProductById(`1`,response.body.name,parseInt(response.body.price)).then((response) => {
+                expect(response.status).to.equal(200)
+            })
         })
     })
 })
@@ -87,14 +97,14 @@ context(`DELETE an product`, () => {
     it(`returns the expected response body properties`, () => {
         cy.postProduct(`TestDelete Item${moment().unix()}`, 12.99).then((response) => {
             cy.deleteProductById(response.body.product_code.toString()).then((response) => {
-                expect(response).property('body').to.exist;
-                expect(response.body).property('product_price').to.be.a('number')
-                expect(response.body).property('name').to.be.a('string')
-                expect(response.body).property('product_code').to.be.a('number')
+                expect(response).property(`body`).to.exist;
+                expect(response.body).property(`product_price`).to.be.a(`number`)
+                expect(response.body).property(`name`).to.be.a(`string`)
+                expect(response.body).property(`product_code`).to.be.a(`number`)
             })
         })
     })
-    it(`returns a 404 if invalid id is sent`,()=>{
+    it(`returns a 404 if invalid id is sent`, () => {
         cy.deleteProductById(`abcd`).then((response) => {
             expect(response.status).to.eq(404);
         })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 Cypress.Commands.add(`getListOfProducts`, () => {
     return cy.request(`GET`, `v1/products`)
 });
@@ -31,13 +6,6 @@ Cypress.Commands.add(`getProductById`, (productId) => {
     return cy.request(`GET`, `v1/product/${productId}`)
 })
 
-Cypress.Commands.add(`putProductById`, (productId) => {
-    return cy.request({
-        url: `v1/product/${productId}`,
-        method: `PUT`,
-        failOnStatusCode: false
-    })
-})
 
 Cypress.Commands.add(`deleteProductById`, (productId) => {
     return cy.request(
@@ -52,6 +20,18 @@ Cypress.Commands.add(`postProduct`, (name, price) => {
     return cy.request({
         url: `v1/product`,
         method: 'POST',
+        body: {
+            name: name,
+            price: price,
+        },
+        failOnStatusCode: false
+    })
+})
+
+Cypress.Commands.add(`putProductById`, (productId, name, price) => {
+    return cy.request({
+        url: `v1/product/${productId}`,
+        method: `PUT`,
         body: {
             name: name,
             price: price,
